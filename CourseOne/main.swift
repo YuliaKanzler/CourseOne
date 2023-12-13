ca8b43e084f0c190b57e53d373524f700e6bc829
Julia Sushko <>
2023-12-14 01:38:21 +0300

    First commit

--- CourseOne/main.swift
+++ CourseOne/main.swift
@@ -7,100 +7,114 @@
 
 import Foundation
 
-//1
+//1 Create an array of days in month
 
-let nameOfPlay = "Basketball"
-let descriptionOfTheGame = "Basketball is a sport in which two teams of five players each try to score points on one another by throwing a ball through a hoop (the basket) under organized rules."
-let players = "Bamba Mo, Beasley Malik, Brown, Troy Jr.,     Christie Max, Davis Anthony, Davis Anthony, Hachimura Rui, Harrison Shaquille, James LeBron, Pippen Scotty Jr.,     Reaves Austin, Russell D'Angelo, Schröder Dennis, Swider Cole, Thompson Tristan, Vanderbilt Jarred, Walker Lonnie IV."
-let foundation = "Team was founded in 1947."
-let city = "Los Angeles"
-let adress = "1111 S. Figueroa Street Los Angeles, CA 90015"
+var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+//2 Count the number of elements in the array
 
-//2
-
-let numberOne = 43
-let numberTwo = 4.830
-let sumOne = numberTwo + Double(numberOne)
-let otherVariantOfSumOne = numberOne + Int(numberTwo)
-
-let number: UInt16 = 1843
-let secondNumber: UInt8 = 43
-let sumOfNumbers = number + UInt16(secondNumber)
-
-let a: Float = 78.4677
-let b = 5.478
-let c = a + Float(b)
-let d = Double(a) + b
-
-let stringNumber = "2345"
-let intNumber = 3456
-let stringNumberToInt = Int(stringNumber)
-let intNimberToString = String(intNumber)
-
-//3
-
-for phrase in "Hello dear friends" {
-    print(phrase)
+var length = 0
+for _ in daysInMonth {
+    length += 1
 }
-        
-for numbers in 1...100{
-    print(numbers)
+print(length)
+
+//3 Print the number of days in each month
+
+for daysPerMonth in daysInMonth {
+    print(daysPerMonth)
 }
 
-for numbers in 1...100{
-    print(numbers * 2)
+//4 Array with month names
+
+let namesOfMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+//5 Print month names with days in month
+
+for index in 0...namesOfMonth.count - 1 {
+    print("\(namesOfMonth[index]), \(daysInMonth[index])")
 }
 
-let names = ["Lisa", "James", "Philipp", "Anna"]
+//6 the same with tuple of arrays
 
-for name in names {
-    print("Hello, \(name)!")
+let tuple = (namesOfMonth, daysInMonth) 
+
+for monthAndDay in 0...tuple.0.count - 1 {
+    print(namesOfMonth[monthAndDay], daysInMonth[monthAndDay])
 }
 
-//4
+//7 Print the days in reverse order
 
-let name = "Yulia"
-let surename = "Kanzler"
-let age = "25"
-let myCity = "Moscow"
-let metroStation = "oktyabrskoe pole"
-let hobbys = "horsriding, fishing, shooting, english learning, travelling"
-let favoriteFilmsGenre = "comedy"
-let occupation = "HR"
-let experience = "5 years"
-let degree = "higher education at State University of management"
-let currentCourses = "english and programming"
+daysInMonth.reverse()
 
-//5
+print(daysInMonth)
 
-let myCV = "Hello, my name is \(name), surename is \(surename). I'm \(age) years old. I live in \(myCity), \(metroStation) metro station. Some of my hobby is \(hobbys). Also I like films, my favorite genre is \(favoriteFilmsGenre). I work as an \(occupation) and have \(experience) experience. I almost have \(degree). Now I do courses - \(currentCourses)."
+//7 From the beginning of the year count the days until the selected date
 
-//6
+let todaysDay = 10
+let todaysMonth = 06
 
-let firstLine = "the sleepless!"
-let firstLineContinue = "Melancholy"
-let secondLine = "glows tremulously far,"
-let thirdLine = "That show’st the darkness"
-let fourthLine = "to joy remember’d well!"
-let fifthLine = "So gleams the past,"
-let sixthLine = "but warms not with its powerless rays;"
-let seventhLine = "A night-beam Sorrow watcheth"
-let eighthLine = "but oh, how cold!"
-let ninthLine = "George Gordon Byron"
 
-let poem = "\t Sun of \(firstLine) \(firstLineContinue) star!\n Whose tearful beam \(secondLine)\n \(thirdLine) thou canst not dispel,\n How like art thou \(fourthLine)\n \t \(fifthLine) the light of other days,\n Which shines, \(sixthLine)\n \(seventhLine) to behold,\n Distinct, but distant — clear, \(eighthLine)\n \t Author - \(ninthLine)"
+var sumOfDays = 0
+for (index, days) in daysInMonth.enumerated() {
+    if index < (todaysMonth - 1) {
+        sumOfDays += days
+    }
+}
 
-print(poem)
+sumOfDays += todaysDay
 
-//7
+print("Количество дней до выбранной даты: \(sumOfDays).")
 
-let myNameIs = "Y\nu\nl\ni\na"
 
-print(myNameIs)
 
-//8
+// Create an array of 12 students boys
 
-let numberOfCars = 390
-let phrase = "Number of cars is "
 
-print(phrase + String(numberOfCars))
+var arrayOfStudents = ["James", "John", "Robert", "Michael", "William", "David", "Richard", "Charles", "Joseph", "Thomas", "Christopher", "Daniel"]
+// Add 7 girls with append and insert
+arrayOfStudents.append("Aurelia")
+arrayOfStudents.append("Eloise")
+arrayOfStudents.append("Luna")
+arrayOfStudents.append("Alice")
+
+arrayOfStudents.insert("Isla", at: 12)
+arrayOfStudents.insert("Sofia", at: 5)
+arrayOfStudents.insert("Kris", at: 5)
+
+print(arrayOfStudents)
+
+// Я понимаю, что можно было сделать через .insert( _:at:) в чем между ними разница с тем, что выбрала я?
+
+// Удалить 7 учеников так, чтобы получилось 5 мальчиков и 5 девочек
+arrayOfStudents.removeSubrange(5..<14)
+print(arrayOfStudents)
+
+//Создать массив оценок от 0 до 5 и второй от 6 до 10. Объединить 2 массива в один
+
+let arrayOfMarks = [0, 1, 2, 3, 4, 5]
+let arrayOfAnotherMarks = [6, 7, 8, 9, 10]
+let arrayOfAllMarks = arrayOfMarks + arrayOfAnotherMarks
+
+
+for (indexx, score) in arrayOfAllMarks.enumerated() {
+
+for (index, student) in arrayOfStudents.enumerated() {
+        if indexx != index {
+            continue
+        }
+        print("\(student) - \(score) баллов. Неплохо!")
+
+    }
+}
+
+// Создайте массив из чисел, числа - это купюры в долларах. Посчитать количество денег и вывести на консоль.
+
+let arrayOfDollars = [35, 742, 24, 84, 47, 93, 562, 44, 87, 78, 67, 92, 976, 573, 68]
+
+var sumOfMoney = 0
+
+for money in arrayOfDollars {
+    sumOfMoney += money
+}
+
+print(sumOfMoney)

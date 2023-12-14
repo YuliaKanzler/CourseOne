403eaa574459a73dbc93b9c63c19c6b3ec6bc505
Julia Sushko <>
2023-12-15 00:51:13 +0300

    Fifth commit

--- CourseOne/main.swift
+++ CourseOne/main.swift
@@ -7,114 +7,75 @@
 
 import Foundation
 
-//1 Create an array of days in month
+// 1. Создать 10 разных Dictionary с разными типами данных. Один из них должен содержать все месяца года на русском. Второй на английском. Соберите все ключи и значения каждого Dictionary и распечатайте в консоль.
 
-var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-//2 Count the number of elements in the array
+let languagesInCountries = ["France": "French", "USA": "English", "Mexico": "Spanish", "UAE": "Arabian", "Canada": "English", "Seychelles": "Creole"]
 
-var length = 0
-for _ in daysInMonth {
-    length += 1
+let carsRegionsInRussia = ["Moscow": 799, "Vladimir": 33, "Orel": 57, "Pskov": 61, "Saint-Petersburg": 78]
+
+let httpStatusCodes = [404: "Not Found", 201: "Webpage Created", 403: "Access allowed only for registered users", 507: "Insufficient Storage", 102: "Processing", 505: "HTTP Version Not Supported", 520: "Unknown Error"]
+
+let ageOfDifferentAnimals = ["Elephant": 40, "Crocodile": 50, "Wolf": 15, "Turtle": 150, "Shark": 30, "Whale": 100]
+
+let describePerson = ["Mom's name": "Iren", "Surname": "Devis", "Age": "44", "Phone": "Iphone", "Degree": "Higher", "Language": "German", "Car": "Ford"]
+
+let averageWeatherInNewYork = ["January": 2.3, "February": 3.6, "March": 7.7, "April": 14.3, "May": 20.6, "June": 26.1, "July": 30.3, "August": 29.1, "September": 24.9, "October": 17.9, "November": 10.7, "December": 5.9]
+
+let salariesOfDifferentProffesions = ["Engineer": 5738, "Lawyer": 11300, "Doctor": 14730, "Accountant": 4370, "Programmer": 12500]
+
+let guessWhatObject = ["Transport": false, "Human": false, "Heavy": true, "Building": false, "Moving": true, "Animal": true, "Fast": false, "Slow": true, "Elephant": false, "Turtle": true]
+
+let monthsInAYearInRussian = [01: "January", 02: "February", 03: "March", 04: "April", 05: "May", 06: "June", 07: "July", 08: "August", 09: "September", 10: "October", 11: "November", 12: "December"]
+
+let monthsInAYearInEnglish = [01: "Январь", 02: "Февраль", 03: "Март", 04: "Апрель", 05: "Май", 06: "Июнь", 07: "Июль", 08: "Август", 09: "Сентябрь", 10: "Октябрь", 11: "Ноябрь", 12: "Декабрь"]
+
+print(languagesInCountries)
+print(carsRegionsInRussia)
+print(httpStatusCodes)
+print(ageOfDifferentAnimals)
+print(describePerson)
+print(averageWeatherInNewYork)
+print(salariesOfDifferentProffesions)
+print(guessWhatObject)
+print(monthsInAYearInRussian)
+print(monthsInAYearInEnglish)
+
+//2.Создайте пустой Dictionary и через условный оператор if проверьте пустой он или нет. Если пустой, то в условии добавьте в него пару значений ?.
+
+var emptyDictionary = [String: String]()
+
+if emptyDictionary.isEmpty {
+    emptyDictionary["Question"] = "??"
 }
-print(length)
+print(emptyDictionary)
 
-//3 Print the number of days in each month
+//3.Создайте словарь - ключ: "Иномарка", "Значение": название машины. Добавьте в него значение по ключу "отечественная". Распечатайте ключи и значения в консоль. Затем отдельно только значения. Удалите из словаря иномарку по ключу (через nil и removeValue()).
 
-for daysPerMonth in daysInMonth {
-    print(daysPerMonth)
-}
+var typesOfCars = ["Foreign": "Ford"]
+typesOfCars["Domestic"] = "Lada"
 
-//4 Array with month names
+print(typesOfCars)
 
-let namesOfMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+print(typesOfCars.values)
 
-//5 Print month names with days in month
+typesOfCars["Foreign"] = nil
+typesOfCars.removeValue(forKey: "Foreign")
 
-for index in 0...namesOfMonth.count - 1 {
-    print("\(namesOfMonth[index]), \(daysInMonth[index])")
-}
+print(typesOfCars)
 
-//6 the same with tuple of arrays
+//Создайте словарь, где ключ - фамилия солдата, а значение - его приветствие. В цикле пройдитесь по всем ключам и распечатайте фамилию каждого солдата. Сделайте тоже самое со значениями и распечатайте приветствие каждого солдата.
 
-let tuple = (namesOfMonth, daysInMonth) 
+let soldiersGreeting = ["Smith": "Hello!", "Devis": "Hi there!", "Braun": "Morning, Sir!", "Jonson": "My respect!", "Moore": "Have a good day, Captain!"]
 
-for monthAndDay in 0...tuple.0.count - 1 {
-    print(namesOfMonth[monthAndDay], daysInMonth[monthAndDay])
-}
-
-//7 Print the days in reverse order
-
-daysInMonth.reverse()
-
-print(daysInMonth)
-
-//7 From the beginning of the year count the days until the selected date
-
-let todaysDay = 10
-let todaysMonth = 06
-
-
-var sumOfDays = 0
-for (index, days) in daysInMonth.enumerated() {
-    if index < (todaysMonth - 1) {
-        sumOfDays += days
-    }
-}
-
-sumOfDays += todaysDay
-
-print("Количество дней до выбранной даты: \(sumOfDays).")
-
-
-
-// Create an array of 12 students boys
-
-
-var arrayOfStudents = ["James", "John", "Robert", "Michael", "William", "David", "Richard", "Charles", "Joseph", "Thomas", "Christopher", "Daniel"]
-// Add 7 girls with append and insert
-arrayOfStudents.append("Aurelia")
-arrayOfStudents.append("Eloise")
-arrayOfStudents.append("Luna")
-arrayOfStudents.append("Alice")
-
-arrayOfStudents.insert("Isla", at: 12)
-arrayOfStudents.insert("Sofia", at: 5)
-arrayOfStudents.insert("Kris", at: 5)
-
-print(arrayOfStudents)
-
-// Я понимаю, что можно было сделать через .insert( _:at:) в чем между ними разница с тем, что выбрала я?
-
-// Удалить 7 учеников так, чтобы получилось 5 мальчиков и 5 девочек
-arrayOfStudents.removeSubrange(5..<14)
-print(arrayOfStudents)
-
-//Создать массив оценок от 0 до 5 и второй от 6 до 10. Объединить 2 массива в один
-
-let arrayOfMarks = [0, 1, 2, 3, 4, 5]
-let arrayOfAnotherMarks = [6, 7, 8, 9, 10]
-let arrayOfAllMarks = arrayOfMarks + arrayOfAnotherMarks
-
-
-for (indexx, score) in arrayOfAllMarks.enumerated() {
-
-for (index, student) in arrayOfStudents.enumerated() {
-        if indexx != index {
-            continue
-        }
-        print("\(student) - \(score) баллов. Неплохо!")
-
-    }
+for surnames in soldiersGreeting.keys {
+    print("Surname is \(surnames)")
 }
 
-// Создайте массив из чисел, числа - это купюры в долларах. Посчитать количество денег и вывести на консоль.
-
-let arrayOfDollars = [35, 742, 24, 84, 47, 93, 562, 44, 87, 78, 67, 92, 976, 573, 68]
-
-var sumOfMoney = 0
-
-for money in arrayOfDollars {
-    sumOfMoney += money
+for greeting in soldiersGreeting.values {
+    print("My greeting is \(greeting)")
 }
 
-print(sumOfMoney)
+for (surname, greeting) in soldiersGreeting {
+    print("Hello! I'm \(surname), and I tell you - \(greeting)")
+}
+
